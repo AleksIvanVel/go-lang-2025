4e33c86053b12fdf6dfb49797ddbacb0e7992c90
Aleks Velazquez <velalmanez@gmail.com>
2025-02-25 12:42:10 -0600

    ADD uso de variables

--- src/firstapp/hola.go
+++ src/firstapp/hola.go
@@ -8,9 +8,38 @@ import (
 	"rsc.io/quote"
 )
 
+// declaracion e variables
+var nombre, apellido string
+var edad int
+
 func main() {
 	fmt.Println("hola mundo")
 
 	//funcion del paquete externo
 	fmt.Println(quote.Hello())
+
+	//otra forma de declarar e inicializar variables en una sola linea
+	var (
+		carrera  = " ICO "
+		semestre = " octavo "
+		promedio = 9.64
+	)
+
+	//otra forma de declarar e inicializar variables en una sola linea
+	/*
+		la palabra reservada "var" perimite declara variables dentro y fuera de las funciones
+	*/
+	var mascota = " lucrecia "
+
+	//otra forma de declarar e inicializar variables en una sola linea
+	/*
+		la sintaxis := solo permite declara variables dentro de funciones
+	*/
+	vehiculo := " RC200 "
+
+	nombre = "Aleks"
+	apellido = " Velazquez "
+	edad = 21
+
+	fmt.Print(nombre, apellido, edad, carrera, semestre, promedio, mascota, vehiculo)
 }
